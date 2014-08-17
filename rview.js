86c61e5a99dd1f2e0cc53add5938e0b188b8a036
syuhei176 <syuhei176@gmail.com>
2014-08-18 02:14:00 +0900

    update

--- rview.js
+++ rview.js
@@ -2,12 +2,8 @@
 	var rview = {
 			views : {},
 			view : function(id, _html, init) {
-				var html = _html;
-				if(typeof html == "object") {
-					html = html.join('');
-				}
 				this.views[id] = {
-						html : html,
+						html : document.getElementById(_html).innerHTML,
 						init : init
 				}
 			},
